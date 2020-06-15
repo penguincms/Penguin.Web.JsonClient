92ec61493c7eeed0e38588d5f227ff873febf834
anloffredo <anloffredo@massageenvy.com>
2020-06-15 15:54:14 -0700

    Auto Commit

--- JsonClient.UploadString.cs
+++ JsonClient.UploadString.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using JsonConvert = Penguin.Json.JsonConvert;
 
 namespace Penguin.Web
 {
